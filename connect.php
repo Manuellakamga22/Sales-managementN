f343ebbbf25d8a923c894f9c4a489028703f0cd0
JedcyChastelle <162721729+JedcyChastelle@users.noreply.github.com>
2024-04-29 16:23:07 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -9,5 +9,5 @@ $db_database	= 'sales';
 
 $db = new PDO('mysql:host='.$db_host.';dbname='.$db_database, $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+/* Ce code a été modifié avec succès */
 ?>
